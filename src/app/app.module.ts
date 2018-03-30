5131c490a371bc573856e9eab3d34e95510bb942
Josh <joshspears93@gmail.com>
2018-03-29 20:20:49 -0400

    Test commit from intellij

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { DeleteModalComponent } from './delete-modal/delete-modal.component';
+import { UpdateModalComponent } from './update-modal/update-modal.component';
+import { NewModalComponent } from './new-modal/new-modal.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    DeleteModalComponent,
+    UpdateModalComponent,
+    NewModalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/new-modal/new-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-modal',
+  templateUrl: './new-modal.component.html',
+  styleUrls: ['./new-modal.component.css']
+})
+export class NewModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
